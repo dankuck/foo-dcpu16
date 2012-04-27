21591eaf29bc4adacf20cc780a7cb18abf24f5ec
unknown <dan@contiguous-jim.(none)>
2012-04-27 00:20:13 -0400

    Scrolls and scrolls.
    This showed that a skipped POP was still POPping. That's fixed now.

--- TestScreen.asm
+++ TestScreen.asm
@@ -2,9 +2,7 @@
 
 :start	SET X, string_to_print
 	JSR print
-	SET X, string_to_print
-	JSR print
-	SET PC, crash
+	SET PC, start
 
 :print	; (X = string pointer)
 
@@ -17,17 +15,38 @@
 	SET [B], A
 	ADD X, 1
 	ADD B, 1
+	IFE B, 0x8180
+		JSR scroll_screen
+	IFG B, 0x8180
+		SET PC, crash
 	SET PC, printloop
 :endprint
 	SET [cursor_position], B
 	SET PC, POP
+
+:scroll_screen
+	SET B, 0x8160
+	SET I, 0x8000
+:scroll_screen_loop
+	SET [I], [32+I]
+	ADD I, 1
+	IFE I, 0x8160
+		SET PC, clear_bottom_line_loop
+	SET PC, scroll_screen_loop
+:clear_bottom_line_loop
+	SET [I], 0
+	ADD I, 1
+	IFE I, 0x8180
+		SET PC, POP
+	SET PC, clear_bottom_line_loop
+	
 	
 	
 :crash
 	DAT 0x0000 ; causes chip to actually crash
 
 :string_to_print
-	DAT "Holy crap it worked.", 0x0
+	DAT "Holy crap it worked! ", 0x0
 
 :cursor_position
 	DAT 0x8000
