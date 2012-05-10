3002083afecf1e035dabcde3f9c000b2a02021bb
unknown <dan@contiguous-jim.(none)>
2012-05-09 21:52:51 -0400

    Parenthesizes the parameters in a macro to ensure that the results match the intent.
    Doesn't parenthesize a parameter if it matches an entire instruction word. So x=>[A] doesn't turn into ([A]). Because that wouldn't work correctly.

--- Assembler.java
+++ Assembler.java
@@ -934,12 +934,15 @@ class Assembler{
 				throw new Exception("Substitutions size doesn't match parameter size");
 			HashMap<String, String> regexes = new HashMap<String, String>();
 			for (int i = 0; i < params.size(); i++)
-				regexes.put("\\b" + params.get(i).replace("\\", "\\\\").replaceAll("([^a-zA-Z0-9])", "\\\\$1") + "\\b", substitutions.get(i));
+				regexes.put("\\b" + params.get(i).replace("\\", "\\\\").replaceAll("([^a-zA-Z0-9])", "\\\\$1") + "\\b", "(" + substitutions.get(i) + ")");
 			FlowFrame frame = new FlowFrame(FlowFrame.MACRO);
 			while (lines.hasMoreLines()){
 				List<String> line = lines.nextLine();
 				for (int i = 0; i < line.size(); i++){
 					//System.out.println("Replacing: " + line.get(i));
+					for (int j = 0; j < params.size(); j++)
+						if (line.get(i).equalsIgnoreCase(params.get(j)))
+							line.set(i, substitutions.get(j));
 					for (Map.Entry<String, String> regex : regexes.entrySet()){
 						//System.out.println(" sub " + regex.getKey() + " with " + regex.getValue());
 						line.set(i, line.get(i).replaceAll(regex.getKey(), regex.getValue()));
