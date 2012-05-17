c7703bc00c07ccbf040f4ac40bb937e2e8fa0484
while1dan <dankuck@gmail.com>
2012-05-17 16:05:36 -0400

    ifdef doesn't check macros.

--- lib/sugar.asm
+++ lib/sugar.asm
@@ -6,21 +6,32 @@
 ; Author: while1dan
 
 
-.ifndef pop
+.ifndef pop_macro
+.def pop_macro 1
 .macro pop(target){
 	SET target, POP
 }
 .end
 
 
-.ifndef push
+.ifndef push_macro
+.def push_macro 1
 .macro push(target){
 	SET PUSH, target
 }
 .end
 
 
-.ifndef return
+.ifndef peek_macro
+.def peek_macro 1
+.macro peek(target){
+	SET target, [SP]
+}
+.end
+
+
+.ifndef return_macro
+.def return_macro 1
 .macro return(){
 	SET PC, POP
 }
